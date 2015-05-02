0a25f322d450c5d2f234f23eecccc5ba6fcbf576
kerhuon <nicolaslerohellec@gmail.com>
2015-05-02 19:02:52 +0200

    Style with Bootstrap

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,4 @@
-app.controller("BillCtrl", function($scope){
+app.controller("BMICtrl", function($scope){
     
     $scope.poids = 70;
     $scope.taille = 175;
@@ -9,13 +9,13 @@ app.controller("BillCtrl", function($scope){
     
     $scope.profil=function(){
         var truc="";
-        if ($scope.imc()<16.5){truc="Dénutrition ou famine";$scope.myVar="class-red"}
-        else if($scope.imc()<=18.5){truc="Maigreur";$scope.myVar="class-orange-plus"} 
+        if ($scope.imc()<16.5){truc="Dénutrition ou famine";$scope.myVar="class-red-plus"}
+        else if($scope.imc()<=18.5){truc="Maigreur";$scope.myVar="class-orange"} 
         else if($scope.imc()<=25){truc="Corpulence normale";$scope.myVar="class-green"}
         else if($scope.imc()<=30){truc="Surpoids";$scope.myVar="class-orange"}
-        else if($scope.imc()<=35){truc="Obésité modérée";$scope.myVar="class-orange-plus"}
-        else if($scope.imc()<=40){truc="Obésité sévère";$scope.myVar="class-red"}
-        else {truc="Obésité morbide ou massive";$scope.myVar="class-red"}
+        else if($scope.imc()<=35){truc="Obésité modérée";$scope.myVar="class-red"}
+        else if($scope.imc()<=40){truc="Obésité sévère";$scope.myVar="class-red-plus"}
+        else {truc="Obésité morbide ou massive";$scope.myVar="class-red-plus"}
         return truc;
     };
 
